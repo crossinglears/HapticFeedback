cd748146d94b70b87fe005007be0e144712b2075
jailx365 <battzSHOT11@gmail.com>
2026-06-27 06:52:16 +0800

    Update HapticFeedback.cs

--- Runtime/Scripts/HapticFeedback.cs
+++ Runtime/Scripts/HapticFeedback.cs
@@ -2,43 +2,50 @@
 
 namespace CrossingLears.HapticFeedback
 {
-
     public static class HapticFeedback
     {
+        public static bool HapticFeedbackEnabled = true;
 
         public static void LightFeedback()
         {
+            if (!HapticFeedbackEnabled)
+            {
+                return;
+            }
 
 #if UNITY_IOS && !UNITY_EDITOR
             iOS.HapticFeedback.PerformHapticFeedback("light");
 #elif UNITY_ANDROID && !UNITY_EDITOR
             Android.HapticFeedback.PerformHapticFeedback(Android.HapticFeedbackConstants.CONTEXT_CLICK);
 #endif
-
         }
 
         public static void MediumFeedback()
         {
+            if (!HapticFeedbackEnabled)
+            {
+                return;
+            }
 
 #if UNITY_IOS && !UNITY_EDITOR
             iOS.HapticFeedback.PerformHapticFeedback("medium");
 #elif UNITY_ANDROID && !UNITY_EDITOR
             Android.HapticFeedback.PerformHapticFeedback(Android.HapticFeedbackConstants.VIRTUAL_KEY);
 #endif
-
         }
 
         public static void HeavyFeedback()
         {
+            if (!HapticFeedbackEnabled)
+            {
+                return;
+            }
 
 #if UNITY_IOS && !UNITY_EDITOR
             iOS.HapticFeedback.PerformHapticFeedback("heavy");
 #elif UNITY_ANDROID && !UNITY_EDITOR
             Android.HapticFeedback.PerformHapticFeedback(Android.HapticFeedbackConstants.LONG_PRESS);
 #endif
-
         }
-
     }
-
 }
